ff39f9aaecce1256ee3b171a5b7013199b5693f1
ZionsAllianceDev <zionsalliancegaming@gmail.com>
2016-03-01 22:39:03 -0800

    Musically++.h
    This Is The Header File With All The Notes

---
+++ Musically.h
@@ -0,0 +1,183 @@
+#pragma once
+#include <windows.h>
+#ifndef Musically_H
+#define Musically_H
+//Normal Notes
+int A(void)
+{
+	Beep(440, 500);
+	return 0;
+}
+int AS(void)
+{
+	Beep(466.164, 500);
+	return 0;
+}
+int B(void)
+{
+	Beep(493.883, 500);
+	return 0;
+}
+int C(void)
+{
+	Beep(523.251, 500);
+	return 0;
+}
+int CS(void)
+{
+	Beep(554.365, 500);
+	return 0;
+}
+int D(void)
+{
+	Beep(587.330, 500);
+	return 0;
+}
+int DS(void)
+{
+	Beep(622.254, 500);
+	return 0;
+}
+int E(void)
+{
+	Beep(658.255, 500);
+	return 0;
+}
+int F(void)
+{
+	Beep(698.456, 500);
+	return 0;
+}
+int FS(void)
+{
+	Beep(739.989, 500);
+	return 0;
+}
+int G(void)
+{
+	Beep(783.991, 500);
+	return 0;
+}
+int GS(void)
+{
+	Beep(830.609, 500);
+	return 0;
+}
+//Short Notes
+int SA(void)
+{
+	Beep(440, 250);
+	return 0;
+}
+int SAS(void)
+{
+	Beep(466.164, 250);
+	return 0;
+}
+int SB(void)
+{
+	Beep(493.883, 250);
+	return 0;
+}
+int SC(void)
+{
+	Beep(523.251, 250);
+	return 0;
+}
+int SCS(void)
+{
+	Beep(554.365, 250);
+	return 0;
+}
+int SD(void)
+{
+	Beep(587.330, 250);
+	return 0;
+}
+int SDS(void)
+{
+	Beep(622.254, 250);
+	return 0;
+}
+int SE(void)
+{
+	Beep(658.255, 250);
+	return 0;
+}
+int SF(void)
+{
+	Beep(698.456, 250);
+	return 0;
+}
+int SFS(void)
+{
+	Beep(739.989, 250);
+	return 0;
+}
+int SG(void)
+{
+	Beep(783.991, 250);
+	return 0;
+}
+int SGS(void)
+{
+	Beep(830.609, 250);
+	return 0;
+}
+//Long Notes
+int LA(void)
+{
+	Beep(440, 750);
+	return 0;
+}
+int LAS(void)
+{
+	Beep(466.164, 750);
+	return 0;
+}
+int LB(void)
+{
+	Beep(493.883, 750);
+	return 0;
+}
+int LC(void)
+{
+	Beep(523.251, 750);
+	return 0;
+}
+int LCS(void)
+{
+	Beep(554.365, 750);
+	return 0;
+}
+int LD(void)
+{
+	Beep(587.330, 750);
+	return 0;
+}
+int LE(void)
+{
+	Beep(658.255, 750);
+	return 0;
+}
+int LF(void)
+{
+	Beep(698.456, 750);
+	return 0;
+}
+int LFS(void)
+{
+	Beep(739.989, 750);
+	return 0;
+}
+int LG(void)
+{
+	Beep(783.991, 750);
+	return 0;
+}
+int LGS(void)
+{
+	Beep(830.609, 750);
+	return 0;
+}
+#endif
